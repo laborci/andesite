7db5478b02032bd52c39155f688f219b37e15110
laborci <gergely@laborci.hu>
2020-04-09 15:12:46 +0200

    new ssl / fpm vhost generator

--- src/CliCommand/GenerateConfig.php
+++ src/CliCommand/GenerateConfig.php
@@ -6,6 +6,9 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Twig\Environment;
+use Twig\Extension\DebugExtension;
+use Twig\Loader\FilesystemLoader;
 
 class GenerateConfig extends CliModule{
 
@@ -20,25 +23,33 @@ class GenerateConfig extends CliModule{
 			protected function configure(){
 				$this->setAliases(['vhost']);
 			}
+
 			protected function execute(InputInterface $input, OutputInterface $output){
 				$style = new SymfonyStyle($input, $output);
 				$env = Env::Service();
 				$files = $this->config;
+				$loader = new FilesystemLoader();
+				$twig = new Environment($loader);
+				$loader->addPath($this->config['source'], '__main__');
 
-				foreach ($files as $name => $file){
-					$source = $file['template'];
-					$target = $file['output'];
 
-					$template = file_get_contents($source);
-					preg_match_all('/\{\{(.*?)\}\}/', $template, $matches);
-					$keys = array_unique($matches[1]);
-					foreach ($keys as $key) $template = str_replace('{{' . $key . '}}', $env->get($key), $template);
-					file_put_contents($target, $template);
-					$style->success($name . ' Done');
+				foreach ($this->config['translate'] as $template => $outfile){
+
+					$output = $twig->render($template.'.twig', $env->get());
+
+					file_put_contents($outfile, $output);
+
+//					$template = file_get_contents($source);
+//					preg_match_all('/\{\{(.*?)\}\}/', $template, $matches);
+//					$keys = array_unique($matches[1]);
+//					foreach ($keys as $key) $template = str_replace('{{' . $key . '}}', $env->get($key), $template);
+//					file_put_contents($target, $template);
+					$style->success($template . ' Done');
 
 				}
 
 			}
 		};
 	}
+	
 }
