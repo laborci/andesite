08feba871c0d5f61176b8bdae90f2e059f5950f2
Laborci Gergely <gergely@laborci.hu>
2020-09-10 11:56:54 +0200

    ApiResponder beforeExecute annotations

--- src/Mission/Web/Responder/ApiJsonResponder.php
+++ src/Mission/Web/Responder/ApiJsonResponder.php
@@ -2,12 +2,27 @@
 
 use Minime\Annotations\Reader;
 use Andesite\Core\ServiceManager\ServiceContainer;
+use ReflectionClass;
+use ReflectionMethod;
 
 abstract class ApiJsonResponder extends JsonResponder{
 
 	protected $method;
 
-	final public function __invoke($action = 'respond'){
+	public final function __invoke($action = 'respond'){
+
+		$beforeExecutes = [];
+
+		/** @var \Minime\Annotations\Reader $reader */
+		$reader = ServiceContainer::get(Reader::class);
+		$ref = new ReflectionClass(get_class($this));
+		$methods = $ref->getMethods(ReflectionMethod::IS_PROTECTED);
+		foreach ($methods as $method){
+			if($reader->getMethodAnnotations($method->class, $method->name)->has('beforeExecution')){
+				$beforeExecutes[] = $method->name;
+			}
+		}
+
 		$response = $this->getResponse();
 		$response->headers->set('Content-Type', 'application/json');
 
@@ -22,6 +37,10 @@ abstract class ApiJsonResponder extends JsonResponder{
 		if(!method_exists($this, $action) || ($methods && !in_array($method, $methods))){
 			$this->getResponse()->setStatusCode(404);
 		}else{
+			$annotations = $reader->getMethodAnnotations(get_class($this), $action);
+			foreach ($beforeExecutes as $beforeExecute){
+				if($this->$beforeExecute($annotations, $action) === false) return;
+			}
 			$response->setContent(json_encode($this->$action(...$this->getArgumentsBag()->all()), JSON_UNESCAPED_UNICODE));
 			$this->next();
 		}
