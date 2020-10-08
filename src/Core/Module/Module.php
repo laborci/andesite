a3b038332fadb101cc7f14e478479018bd65d839
laborci <gergely@laborci.hu>
2020-10-08 20:49:16 +0200

    modul docblock updated

--- src/Core/Module/Module.php
+++ src/Core/Module/Module.php
@@ -3,5 +3,9 @@
 use Andesite\Core\ServiceManager\SharedService;
 
 abstract class Module implements SharedService{
+	/**
+	 * @param bool $loaded
+	 * @return static|null
+	 */
 	public static function Module($loaded = true): ?self{ return ModuleManager::get(get_called_class(), $loaded); }
 }
