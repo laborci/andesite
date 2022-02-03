ef2ebc9272978b7b286b3e61364be2c4b4f0b4ea
Gergely Laborci <gergely@laborci.hu>
2022-02-03 11:38:29 +0100

    reroute fix

--- src/Mission/Reroute/RerouteMission.php
+++ src/Mission/Reroute/RerouteMission.php
@@ -1,14 +1,12 @@
 <?php namespace Andesite\Mission\Reroute;
 
 use Andesite\Mission\Mission;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class RerouteMission extends Mission{
 
 	protected $config;
 	protected function run($config){
-		(new RedirectResponse('location:' . Request::createFromGlobals()->getScheme() . '://' . $config['reroute'], 301))->send();
-		die();
+		die(header('location:' . Request::createFromGlobals()->getScheme() . '://' . $config['reroute']));
 	}
 }
